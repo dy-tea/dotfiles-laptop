18e6fcfc1e8e81875d88c491ecf9d6960fc1cb79
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-03-27 18:33:44 +0000

    feat: add awm and waybar configs

---
+++ waybar/workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+awmsg -c -1 w l | while IFS= read -r line; do
+	active_workspace=$(echo "$line" | jq '.active')
+	if [ "$active_workspace" != "null" ]; then
+		echo "{\"text\": \"$active_workspace\", \"class\": \"awm_workspace\", \"tooltip\": \"Active Workspace: $active_workspace\"}"
+	fi
+done
