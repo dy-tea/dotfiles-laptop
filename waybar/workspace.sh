3f7a0eaedb7319c5e3aee3a679875d5b0913013f
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-04-07 11:31:36 +0100

    feat: add file picker command

--- waybar/workspace.sh
+++ waybar/workspace.sh
@@ -5,3 +5,4 @@ awmsg -c -1 w l | while IFS= read -r line; do
 		echo "{\"text\": \"$active_workspace\", \"class\": \"awm_workspace\", \"tooltip\": \"Active Workspace: $active_workspace\"}"
 	fi
 done
+
